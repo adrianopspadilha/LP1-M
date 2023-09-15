a5aa4102751eb46d3dc6e782932c62ac1b536454
adrianopspadilha <adrianopspadilha@gmail.com>
2023-09-14 23:22:15 -0300

    Initial commit

---
+++ src/animal.java
@@ -0,0 +1,66 @@
+public class animal {
+    private String nome;
+    private String caracteristica;
+    private int numDePatas;
+    private String cor;
+    private String ambiente;
+    private double velocidadeMedia;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCaracteristica() {
+        return caracteristica;
+    }
+
+    public void setCaracteristica(String caracteristica) {
+        this.caracteristica = caracteristica;
+    }
+
+    public int getNumDePatas() {
+        return numDePatas;
+    }
+
+    public void setNumDePatas(int numDePatas) {
+        this.numDePatas = numDePatas;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getAmbiente() {
+        return ambiente;
+    }
+
+    public void setAmbiente(String ambiente) {
+        this.ambiente = ambiente;
+    }
+
+    public double getVelocidadeMedia() {
+        return velocidadeMedia;
+    }
+
+    public void setVelocidadeMedia(double velocidadeMedia) {
+        this.velocidadeMedia = velocidadeMedia;
+    }
+
+    public animal (String nome, String caracteristica, int numDePatas, String cor, String ambiente, double velocidadeMedia){
+        this.nome = nome;
+        this.caracteristica = caracteristica;
+        this.numDePatas = numDePatas;
+        this.cor = cor;
+        this.ambiente = ambiente;
+        this.velocidadeMedia = velocidadeMedia;
+
+    }
+}
---
+++ src/peixe.java
@@ -0,0 +1,16 @@
+public class peixe {
+
+    private String caracteristica;
+
+    public animal (String nome, String caracteristica, int numDePatas, String cor, String ambiente, double velocidadeMedia){
+        this.nome = nome;
+        this.caracteristica = caracteristica;
+        this.numDePatas = numDePatas;
+        this.cor = cor;
+        this.ambiente = ambiente;
+        this.velocidadeMedia = velocidadeMedia;
+
+    }
+
+
+}
